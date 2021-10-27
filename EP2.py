7da4408a27a29153a01532de9cb8405fdf3a3c2a
Lucas <lucastl1@al.insper.edu.br>
2021-10-27 19:20:43 -0300

    Cria peças

--- EP2.py
+++ EP2.py
@@ -1,3 +1,25 @@
+import random
+def cria_pecas():
+    pecas = []
+    i = 0
+    j = 0
+    c = 1
+    m = 0
+    while m < 28:
+        if i > 6:
+            i = 0
+            i += c
+            c += 1
+            j += 1        
+        pecas.append([j,i])
+        m += 1
+        i += 1
+
+    random.shuffle(pecas)
+    return pecas
+
+
+
 print('Bem-vindo(a) ao jogo de Dominó! O objetivo desse jogo é ficar sem peças na sua mão antes dos outros jogadores.')
 
 print('Vamos começar!!')
